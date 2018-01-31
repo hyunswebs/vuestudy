7d408f1510b6f87888d9f00365978a185e01dc67
min su kong <mansukong@gmail.com>
2018-01-31 20:20:48 +0900

    view simple end

--- vuejs/src/View.vue
+++ vuejs/src/View.vue
@@ -1,5 +1,13 @@
 <template>
 <transition name="fade">
-    <div>view</div>  
+    <div class="view-wrap">
+        <h2>view</h2>
+        <div class="demo">
+            view page demo 입니다.
+        </div>
+    </div>  
 </transition>
 </template>
+<style lang="scss">
+   @import './assets/css/layout.scss'
+</style>
