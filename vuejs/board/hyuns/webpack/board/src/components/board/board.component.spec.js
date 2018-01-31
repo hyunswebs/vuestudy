8008ff0563d524c37b5e3ac7e0acd591bed953a7
hyuns <hyunswebs@naver.com>
2018-01-31 17:51:29 +0900

    2018/01/31 Study - hyuns

--- vuejs/board/hyuns/webpack/board/src/components/board/board.component.spec.js
+++ vuejs/board/hyuns/webpack/board/src/components/board/board.component.spec.js
@@ -1,26 +1,25 @@
 import Vue from 'vue';
 import BoardComponent from './index.vue';
 
-// Here are some Jasmine 2.0 tests, though you can
-// use any test runner / assertion library combo you prefer
+// Jasmine 2.0 테스트는 다음과 같습니다.
+// 원하는 테스트 러너 / 테스트 라이브러리를 사용하십시오
 describe('BoardComponent', () => {
-  // Inspect the raw component options
+  // 원시 컴포넌트 옵션을 검사합니다.
   it('has a created hook', () => {
     // expect(typeof BoardComponent.created).toBe('function');
   })
-  // Evaluate the results of functions in
-  // the raw component options
+  // 원시 컴포넌트 옵션에서 함수 결과를 테스트합니다.
   it('sets the correct default data', () => {
     // expect(typeof BoardComponent.data).toBe('function')
     // const defaultData = BoardComponent.data();
     // expect(defaultData.message).toBe('hello!');
   })
-  // Inspect the component instance on mount
+  // 마운트 할 때 컴포넌트 인스턴스를 검사합니다.
   it('correctly sets the message when created', () => {
     // const vm = new Vue(BoardComponent).$mount();
     // expect(vm.message).toBe('bye!');
   })
-  // Mount an instance and inspect the render output
+  // 인스턴스를 마운트하고 출력된 결과를 검사합니다.
   it('renders the correct message', () => {
     // const Ctor = Vue.extend(BoardComponent);
     // const vm = new Ctor().$mount();
